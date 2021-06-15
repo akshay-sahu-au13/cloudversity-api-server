4cbf2f9bbb0b0423a7dceb0bd3c4aa825a058efe
Akshay Sahu <akki5233@gmail.com>
2021-06-15 15:13:03 +0530

    Populated the wishlistedBy array inside course routes

--- Routes/course.js
+++ Routes/course.js
@@ -79,6 +79,7 @@ Router.get("/course/:courseId", async (req, res) => {
             .populate("reviews", ["reviewBody", "rating"])
             .populate("videos", ["videoLink", "title", "publicId"])
             .populate("authorName", ["firstName", "lastName"])
+            .populate("wishlistedBy")
             .exec();
 
         res.status(200).send({ requestedCourse });
