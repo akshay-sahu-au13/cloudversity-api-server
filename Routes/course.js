1d3fa680eb36480c616117026a4eb0000110af2c
Akshay Sahu <akki5233@gmail.com>
2021-06-21 20:11:54 +0530

    Added all the course routes to Course Controllers

--- Routes/course.js
+++ Routes/course.js
@@ -5,12 +5,10 @@ const Course = require("../Model/course");
 const Video = require("../Model/video");
 const Tutor = require("../Model/tutor");
 const Student = require("../Model/student");
-const bufferConversion = require("../Utils/bufferConversion");
-const { imageUpload, videoUpload } = require("../Utils/multer");
 const { cloudinary } = require("../Utils/clodinary");
 
 
-const { addCourse, getAllCourses, getSingleCourse } = require("../Controllers/courseControllers");
+const { addCourse, getAllCourses, getSingleCourse, deleteCourse, enrollCourse, updateCourse, uploadVideo, deleteVideo, applyDiscount, updateThumbnail } = require("../Controllers/courseControllers");
 
 //-------------- COURSE AND VIDEO ROUTES BELOW ---------------- //
 
@@ -25,187 +23,25 @@ Router.get("/allcourses", getAllCourses);
 Router.get("/course/:courseId", getSingleCourse);
 
 // ------------------- DELETE: Delete a particular Course ---------------//
-Router.delete("/deletecourse/:courseId", auth, (req, res) => {
-    try {
-        const course = await Course.findById({ _id: req.params.courseId }).populate("videos", ["publicId"]);
-
-        for (let i = 0; i < course.videos.length; i++) {
-            if (course.videos[i].publicId) {
-                await cloudinary.uploader.destroy(course.videos[i].publicId, { resource_type: 'video', upload_preset: "cloudversity-dev" });
-            };
-        };
-
-        await Course.findOneAndDelete({ _id: course._id });
-
-        res.status(200).send({ message: "Course and its content deleted successfully", DeletedCOurse: course });
-
-    } catch (error) {
-        console.log("Error occurred while deleting the course...", error);
-        res.status(500).send({ message: "Couldn't delete the course", error: error.message });
-    };
-});
+Router.delete("/deletecourse/:courseId", auth, deleteCourse);
 
 // ------------------- POST: Enroll to Course ---------------//
-Router.post("/enroll/:courseId", auth, async (req, res) => {
-
-    try {
-
-        const course = await Course.findById({ _id: req.params.courseId });
-        const student = await Student.findById({ _id: req.user.id });
-        const tutor = await Tutor.findById({ _id: course.authorName});
-
-        if (!course.enrolledStudents.includes(req.user.id)) {
-
-            course.enrolledStudents.push(req.user.id);                 // updating the enrolled list of course
-            student.enrolledCourses.push(req.params.courseId);         // updating the enrolled list of student
-            dicountedPrice = (course.price * course.discount) / 100;   // applying discount on the course
-            tutor.totalEarnings += dicountedPrice.toFixed(2);          // updating total earnings of the tutor
-            
-            await course.save();
-            await student.save();
-            await tutor.save();      
-
-            res.status(200).send({ message: "New course enrolled successfully", enrolledCourses: student.enrolledCourses });
-        }
-        res.status(200).send({message: "Student already enrolled to this course"});
-
-    } catch (error) {
-        console.log("Error occurred while enrolling...", error);
-        res.status(500).send({ message: "Couldn't enroll to the course", error: error.message });
-    };
-
-});
+Router.post("/enroll/:courseId", auth, enrollCourse);
 
 // ------------------- PATCH: Course details Update ---------------//
-Router.patch("/updatecourse/:courseId", auth, async (req, res) => {
-
-    try {
-
-        const updatedDetails = {
-            ...req.body
-        }
-        const updatedCourse = await Course.findOneAndUpdate({ _id: req.params.courseId }, {
-            $set: updatedDetails
-        });
-
-        res.status(200).send({ message: "Course details updated successfully!", updatedDetails });
-
-
-    } catch (error) {
-        console.log("Error occurred while updating...", error);
-        res.status(500).send({ message: "Couldn't update the course", error: error.message });
-    };
-});
+Router.patch("/updatecourse/:courseId", auth, updateCourse);
 
 
 // ------------------- PATCH: Course Thumbnail Update ---------------//
-Router.patch("/updatethumbnail/:courseId", auth, imageUpload.single('thumbnail'), async (req, res) => {
-
-    try {
-
-        const convertedBuffer = await bufferConversion(req.file.originalname, req.file.buffer);
-
-        const uploadedImage = await cloudinary.uploader.upload(convertedBuffer, { resource_type: "image", upload_preset: "cloudversity-dev", });
-
-        const thumbnailUpdate = await Course.findOneAndUpdate({ _id: req.params.courseId }, {
-            $set: {
-                thumbnail: uploadedImage.secure_url
-            }
-        });
-
-        res.status(200).send({ message: "thumbnail updated", thumbnail: uploadedImage.secure_url });
-
-    } catch (error) {
-        console.log("Error occurred while updating thumbnail...", error);
-        res.status(500).send({ message: "Couldn't update the thumbnail", error: error.message });
-    };
-});
+Router.patch("/updatethumbnail/:courseId", auth, updateThumbnail);
 
 // ------------------- POST: Upload Videos ---------------//
-Router.post("/uploadvideo/:courseId", auth, async (req, res) => {
-
-    try {
-
-        const video = new Video({
-            title: req.body.title
-        });
-
-        const uploadedVideo = await cloudinary.uploader.upload(req.body.videoLink, { resource_type: "video", upload_preset: "cloudversity-dev", });
-
-        console.log("Uploaded video object: ", uploadedVideo);
-        video.courseId = req.params.courseId;
-        video.authorId = req.user.id;
-
-        videoLength = (uploadedVideo.duration / 60).toFixed(3);    // converting videoLength to minutes
-
-        console.log("Video Length : ", videoLength);
-
-        video.videoLength = videoLength;
-        video.videoLink = uploadedVideo.secure_url;
-        video.publicId = uploadedVideo.public_id.split("/")[1];   // adding public Id to video Schema
-
-        await video.save();
-
-        const course = await Course.findById({ _id: req.params.courseId }).populate("videos", ["videoLength"]);
-        
-        // ----- Logic to calculate the course duration ----- //
-        const courseDuration = course.videos.reduce((sum, video) => {
-            return sum + video.videoLength;
-        }, 0) + parseFloat(videoLength);
-
-        course.courseDuration = courseDuration.toFixed(3);
-        course.videos.push(video._id);
-        await course.save();
-
-        res.status(201).send({ message: "Video uploaded successfully", videoDetails: video });
-
-    } catch (error) {
-
-        console.log("Error occurred while uploading...", error);
-        res.status(500).send({ message: "Couldn't upload the video", error: error.message });
-
-    };
-});
+Router.post("/uploadvideo/:courseId", auth, uploadVideo);
 
 // ------------------- DELETE: Route to Delete a Video ------------------- //
-Router.delete("/deletevideo/:videoId", auth, async (req, res) => {
-    try {
-        
-        const videoToDelete = await Video.findById({_id: req.params.videoId});
-        const course = Course.findById({ _id: videoToDelete.courseId});
-        const deletedVideo = await cloudinary.uploader.destroy(videoToDelete.publicId, { resource_type: 'video', upload_preset: "cloudversity-dev"});
-
-        await Video.findOneAndDelete({_id: req.params.videoId});
-       
-        const indexOfVideo = course.videos.indexOf(req.params.videoId);
-
-        if (indexOfVideo > -1) {
-            course.videos.splice(indexOfVideo, 1);           // removing video from the videos list of course
-            res.status(200).send({ message: "Sucess! The video has been deleted" });            
-        } else {
-            res.status(200).send({ message: "Video not present in course's video list" });
-        };
-        
-    } catch (error) {
-        console.log("Error occurred while deleting the video...", error);
-        res.status(500).send({ message: "Couldn't delete the video, try again", error: error.message });
-    };
-});
+Router.delete("/deletevideo/:videoId", auth, deleteVideo);
 
 // ------------------- PATCH: Apply discount on a Course ---------------//
-Router.patch("/applydiscount/:courseId", auth, async(req, res) => {
-    try {
-        
-        const course  = await Course.findById({_id: req.params.courseId});
-        course.discount = req.body.discount;
-        await course.save();
-
-        res.status(200).send({message: `${req.body.discount}% discount applied on course`, course});
-        
-    } catch (error) {
-        console.log("Error occurred while applying discount...", error);
-        res.status(500).send({ message: "Couldn't apply discount, try again", error: error.message });
-    };
-});
+Router.patch("/applydiscount/:courseId", auth, applyDiscount);
 
 module.exports = Router;
